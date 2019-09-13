3913207a63b3cd51e5f9f08e712d8f1cfec0b05f
Piotrek Rudnicki <p.rudnicki@developerodzera.pl>
2019-09-13 17:31:59 +0200

    Add base systems

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts.Managers
+{
+    public enum SoundType
+    {
+        SFX,
+        MUSIC
+    }
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource sfxSource;
+        [SerializeField]
+        private AudioSource musicSource;
+        [SerializeField]
+        private AudioMixer audioMixer;
+
+        public static SoundManager Instance = null;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void PlaySingle(AudioClip clip)
+        {
+            sfxSource.clip = clip;
+            sfxSource.Play();
+        }
+
+        public void SetVolume(SoundType type, float value)
+        {
+            switch(type)
+            {
+                case SoundType.SFX: audioMixer.SetFloat("SFXVolume", value); break;
+                case SoundType.MUSIC: audioMixer.SetFloat("MusicVolume", value); break;
+            }
+        }
+    }
+}
