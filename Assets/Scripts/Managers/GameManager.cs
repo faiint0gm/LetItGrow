0e2cca0558e795a79c88308d3a0b84f3a3aa824c
Piotrek Rudnicki <p.rudnicki@developerodzera.pl>
2019-09-13 19:13:32 +0200

    Setup instance of GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager Instance = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+        } else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 }
